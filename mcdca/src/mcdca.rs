bdc33f43c389a8f3ad378df8e1d2b01b129a6a55
Dominik Gront <dgront@chem.uw.edu.pl>
2022-07-26 18:47:32 -0400

    [mcdca] Refactoring
    Now couplings are stored in Couplings struct, which also implements delta_energy() and total_energy() methods

--- mcdca/src/mcdca.rs
+++ mcdca/src/mcdca.rs
@@ -1,103 +1,160 @@
 use rand::Rng;
+use std::collections::HashMap;
 
-const N: usize = 150;
-const K: usize = 2;
+use bioshell_core::Sequence;
 
-pub fn init_1d(cplngs: &mut Vec<Vec<f32>>) {
-    for i in 1..N {
-        let ii = i * K;
-        for j in 0..K {
-            cplngs[ii + j][ii - K + j] = -1.0;
-            cplngs[ii - K + j][ii + j] = -1.0;
+// const N: usize = 50;
+// const K: usize = 21;
+
+// pub fn init_diagonal_couplings(cplngs: &mut Vec<Vec<f32>>) {
+//     for i in 1..N {
+//         let ii = i * K;
+//         for j in 0..K {
+//             cplngs[ii + j][ii - K + j] = -1.0;
+//             cplngs[ii - K + j][ii + j] = -1.0;
+//         }
+//     }
+//     for j in 0..K {
+//         cplngs[j][K + j] = -1.0;
+//         cplngs[K + j][j] = -1.0;
+//     }
+// }
+
+pub struct Couplings {
+    pub n: usize,
+    pub k: usize,
+    cplngs: Vec<Vec<f32>>,
+    index_to_aa: Vec<u8>,
+}
+
+impl Couplings {
+    /// Creates an empty Coupling instance i.e. none of amino acids are coupled
+    pub fn new(seq_len: usize, aa_order: &str) -> Couplings {
+        let m = vec![vec![0.0; seq_len * aa_order.len()]; seq_len * aa_order.len()];
+        let index_to_aa = aa_order.as_bytes().to_vec();
+        println!("amino acid order: {:?}",index_to_aa);
+        Couplings { n: seq_len, k: aa_order.len(), cplngs: m, index_to_aa }
+    }
+
+    /// Initializes coupling diagonally.
+    ///
+    pub fn init_couplings_diagonaly(&mut self) {
+        for i in 1..self.n {
+            let ii = i * self.k;
+            for j in 0..self.k {
+                self.cplngs[ii + j][ii - self.k + j] = -1.0;
+                self.cplngs[ii - self.k + j][ii + j] = -1.0;
+            }
+        }
+        for j in 0..self.k {
+            self.cplngs[j][self.k + j] = -1.0;
+            self.cplngs[self.k + j][j] = -1.0;
         }
     }
-    for j in 0..K {
-        cplngs[j][K + j] = -1.0;
-        cplngs[K + j][j] = -1.0;
-    }
-}
 
-pub fn delta_energy(system: &Vec<i8>, cplngs: &Vec<Vec<f32>>, pos: usize, old: usize, new: usize) -> f32 {
-    let mut en: f32 = 0.0;
-    let mut pos_j: usize = 0;
-    let pos_i: usize = pos * K;
-    for aa_j in system.iter() {
-        en += cplngs[pos_j + *aa_j as usize][pos_i + new] - cplngs[pos_j + *aa_j as usize][pos_i + old];
-        // println!("{} {} {} {} {}", pos_j, aa_j, cplngs[pos_j + *aa_j as usize][pos_i + new], cplngs[pos_j + *aa_j as usize][pos_i + old], en);
-        pos_j+=K;
+    /// Initializes coupling from a given set of sequences.
+    ///
+    pub fn init_couplings_by_msa(&mut self, msa: &Vec<Sequence>) {
+        for sequence in msa {
+            todo!()
+        }
     }
 
-    return en;
-}
+    /// Prints the large matrix of couplings on the screen
+    pub fn show_matrix(n:usize, k:usize, m: &Vec<Vec<f32>>) {
+        for (i, row) in m.iter().enumerate() {
+            for (j, val) in row.iter().enumerate() {
+                print!(" {:.3}",val);
+                if j % k == (k - 1) { print!(" ") }
+            }
+            println!("");
+            if i % k == (k - 1) { println!("#") }
+        }
+    }
 
-pub fn total_energy(system: &Vec<i8>, cplngs: &Vec<Vec<f32>>) -> f32 {
-    let mut en:f32 = 0.0;
-    let mut pos_i :usize = 0;
-    for aa_i in system.iter() {
-        let mut pos_j :usize = 0;
+    /// Prints the large matrix of couplings on the screen
+    pub fn show(&self) { Couplings::show_matrix(self.n, self.k, &self.cplngs); }
+
+    pub fn delta_energy(&self, system: &Vec<i8>, pos: usize, old: usize, new: usize) -> f32 {
+        let mut en: f32 = 0.0;
+        let mut pos_j: usize = 0;
+        let pos_i: usize = pos * self.k;
         for aa_j in system.iter() {
-            en += cplngs[pos_i + *aa_i as usize][pos_j + *aa_j as usize];
-            pos_j += K;
+            en += self.cplngs[pos_j + *aa_j as usize][pos_i + new] - self.cplngs[pos_j + *aa_j as usize][pos_i + old];
+            // println!("{} {} {} {} {}", pos_j, aa_j, cplngs[pos_j + *aa_j as usize][pos_i + new], cplngs[pos_j + *aa_j as usize][pos_i + old], en);
+            pos_j+=self.k;
         }
-        pos_i += K;
+
+        return en;
     }
-    return en/2.0;
+
+    pub fn total_energy(&self, system: &Vec<i8>) -> f32 {
+        let mut en:f32 = 0.0;
+        let mut pos_i :usize = 0;
+        for aa_i in system.iter() {
+            let mut pos_j :usize = 0;
+            for aa_j in system.iter() {
+                en += self.cplngs[pos_i + *aa_i as usize][pos_j + *aa_j as usize];
+                pos_j += self.k;
+            }
+            pos_i += self.k;
+        }
+        return en/2.0;
+    }
 }
 
-pub fn accumulate_counts(system: &Vec<i8>, counts: &mut Vec<Vec<f32>>) {
+
+pub fn accumulate_counts(system: &Vec<i8>, n_aa:usize, counts: &mut Vec<Vec<f32>>) {
     let mut pos_i: usize = 0;
     for aa_i in system.iter() {
         let mut pos_j: usize = 0;
         for aa_j in system.iter() {
             counts[pos_i + *aa_i as usize][pos_j + *aa_j as usize] += 1.0;
-            pos_j += K;
+            pos_j += n_aa;
         }
-        pos_i += K;
+        pos_i += n_aa;
     }
 }
 
-pub fn print_matrix(m: &Vec<Vec<f32>>) {
-    for row in m.iter() {
-        for val in row.iter() { print!(" {:.3}",val); }
-        println!("");
-    }
-}
+pub fn isothermal_mc(system: &mut Vec<i8>, energy: &Couplings, inner_cycles: i32, outer_cycles: i32) -> Vec<Vec<f32>>{
 
-pub fn isothermal_mc(system: &mut Vec<i8>, cplngs: &Vec<Vec<f32>>, inner_cycles: i32, outer_cycles: i32) -> Vec<Vec<f32>>{
-
-    let mut counts: Vec<Vec<f32>> = vec![vec![0.0; N * K]; N * K];
+    let mut counts: Vec<Vec<f32>> = vec![vec![0.0; energy.n * energy.k]; energy.n * energy.k];
     let mut rng = rand::thread_rng();
-    let mut total_en:f32 = total_energy(&system,&cplngs);
+    let mut total_en:f32 = energy.total_energy(&system);
     let mut n_obs: f32 = 0.0;
+    let mut n_succ: f32 = 0.0;
     for io in 0..outer_cycles {
         for ii in 0..inner_cycles {
             for is in 0..system.len() {
-                let pos: usize = rng.gen_range(0..N);
-                let new_aa: usize = rng.gen_range(0..K);
-                let delta_en = delta_energy(system, cplngs, pos, system[pos] as usize, new_aa);
+                let pos: usize = rng.gen_range(0..energy.n);
+                let new_aa: usize = rng.gen_range(0..energy.k);
+                let delta_en = energy.delta_energy(system, pos, system[pos] as usize, new_aa);
                 if delta_en > 0.0 && (-delta_en).exp() < rng.gen_range(0.0..1.0) { continue; }
                 system[pos] = new_aa as i8;
                 total_en += delta_en;
+                n_succ += 1.0;
             }
         }
         n_obs += 1.0;
-        accumulate_counts(&system, &mut counts);
+        accumulate_counts(&system, energy.k, &mut counts);
         // println!("{} {:?}",total_en,system);
     }
+    println!("#{}",n_succ / (outer_cycles*inner_cycles*system.len() as i32) as f32);
     counts.iter_mut().for_each(|el| el.iter_mut().for_each(|iel| *iel /= n_obs));
     return counts;
 }
 
 pub fn main() {
-    let mut system: Vec<i8> = vec![0; N];
-    let mut cplngs: Vec<Vec<f32>> = vec![vec![0.0; N * K]; N * K];
+    let seq_len: usize = 10;
+    let mut system: Vec<i8> = vec![0; seq_len];
 
-    init_1d(&mut cplngs);
-    // println!("{:?}", &cplngs);
+    // let mut en: Couplings = Couplings::new(100, "ACDEFGHIKLMNPQRSTVWY-");
+    let mut en: Couplings = Couplings::new(seq_len, "ACD");
 
-    total_energy(&system, &cplngs);
-    // delta_energy(&system, &cplngs,0,0,1);
-    let counts = isothermal_mc(&mut system, &cplngs,100,10000);
-    // println!("{:?}",counts);
-    print_matrix(&counts);
+    en.init_couplings_diagonaly();
+
+    en.total_energy(&system);
+    let counts = isothermal_mc(&mut system, &en,100,1000);
+    en.show();
+    Couplings::show_matrix(en.n, en.k, &counts);
 }
