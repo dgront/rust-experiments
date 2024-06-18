0cb0fa99c52d6fc568e7fdda4b053c138a17a9f0
Dominik Gront <dgront@chem.uw.edu.pl>
2024-06-18 16:46:48 +0200

    [py_lib] Python example shows how to iterate over a (copied) vector of structs

--- py_library_examples/pyshapes/py_examples/demo.py
+++ py_library_examples/pyshapes/py_examples/demo.py
@@ -4,5 +4,10 @@ s.add_vertex( Vertex(1.0, 2.0))
 s.add_vertex( Vertex(3.0, 4.0))
 s.add_vertex( Vertex(5.0, 6.0))
 print(s)
-#cm = s.center()
+
+cm = s.center()
+
+# iteration also works
+for v in s.get_vertices():
+    print(v)
 
